b67543d1801632fe6717cfabbb6f50b7d7257343
alex gian <alexhgian@gmail.com>
2015-04-27 22:50:21 -0700

    Rename

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var gls = require('gulp-live-server');
 
 gulp.task('serve', function() {
     // serve with default settings
-    var server = gls.static('static', 3000); //equals to gls.static('public', 3000);
+    var server = gls.static('app', 3000); //equals to gls.static('public', 3000);
     server.start();
 
     //use gulp.watch to trigger server actions(notify, start or stop)
-    gulp.watch(['static/**/*.js','static/**/*.css', 'static/**/*.html'], server.notify);
+    gulp.watch(['app/**/*.js','app/**/*.css', 'app/**/*.html'], server.notify);
 });
 
 
