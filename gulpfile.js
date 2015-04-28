704ed62b049c47daf18ad10c69eed8340b2cc051
alex gian <alexhgian@gmail.com>
2015-04-27 23:04:58 -0700

    Added Library

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gls = require('gulp-live-server');
 
 gulp.task('serve', function() {
     // serve with default settings
-    var server = gls.static('app', 3000); //equals to gls.static('public', 3000);
+    var server = gls.static(['.','bower_components'],3000); //equals to gls.static('public', 3000);
     server.start();
 
     //use gulp.watch to trigger server actions(notify, start or stop)
